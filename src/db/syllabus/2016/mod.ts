ef7d179f79a3b96ea585f39a9bb6d5c6c0579b86
Abhinandh S <abhinandhsuby@proton.me>
2026-07-12 11:17:52 +0530

    Update mod.ts

--- src/db/syllabus/2016/mod.ts
+++ src/db/syllabus/2016/mod.ts
@@ -1,4 +1,23 @@
 // import { * as syllabus2016 } from '../db/..'
+
 export const _PYQS: Record<string, string> = {
   'p5-21d-pyq': ''
+};
+
+export const _MQPS: Record<string, FileRecord> = {
+  'p5-21d-mqp': [
+    {
+      name: 's1',
+      id: ''
+    },
+  ]
+};
+
+export const _PTPS: Record<string, FileRecord> = {
+  'p5-21d-ptp': [
+    {
+      name: 'q',
+      id: ''
+    },
+  ]
 };
