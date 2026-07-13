33adaa520022545fdd0b69d53f6f05f50566f8df
Abhinandh S <abhinandhsuby@proton.me>
2026-07-13 23:18:44 +0530

    Update render.ts

--- src/render.ts
+++ src/render.ts
@@ -25,3 +25,18 @@ export function renderSet(id: string): string {
 export function renderSyllabus(ctx: string): string {
   return ctx === "syl16" ? "<b>syllabus</b>: 2016" : ctx === "syl22" ? "<b>syllabus</b>: 2022" : ctx;
 }
+
+export function renderCaption(
+name, ):string {
+
+let caption = "";
+
+  //  const header = `${renderLevel(paperId)}\n#${docType.toUpperCase()}`;
+   // const commonCaption = `${header}\n📄 paper: ${paper.name}\n🗂️ paper no: ${paperId.replace("p", "")}\n📆 term: ${formatTerm(term)}`;
+
+  // if (docType === "pyq") {
+      
+        //  caption: `${commonCaption}\n🗄️ ${formatSet(file.name)}`,
+
+return caption;
+}
