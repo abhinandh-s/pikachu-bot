963be2c94334a006c691c4c7b1802c8b781c0a48
Abhinandh S <abhinandhsuby@proton.me>
2026-07-13 13:44:53 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,6 @@ import { inlineQueryHandler } from "./inline.ts";
 import { formatTerm } from "./utils.ts";
 import { resolve } from "@std/path";
 
-import { migrationCmd } from "./migration.ts";
-
 const bot = new Bot(Deno.env.get("TELEGRAM_TOKEN") || "");
 
 bot.use(helpCmd);
