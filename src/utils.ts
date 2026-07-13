57eccd066d591aab8c9e7d31a49a6456bf1eff65
Abhinandh S <abhinandhsuby@proton.me>
2026-07-13 12:27:11 +0530

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -9,3 +9,15 @@ export function formatTerm(code: string): string {
 
   return `${year} ${month}`;
 }
+
+export function renderLevel(id: string): string {
+  // "p7"
+  //   ^
+  const n = code.slice(1); 
+
+  const res = n > 12 ? 'CMA FINAL' : n > 4 ? 'CMA INTERMEDIATE' : 'CMA FOUNDATION';
+
+  return `<blockqoute>${year}</blockqoute>`;
+}
+
+console.log(`p4 = `)
