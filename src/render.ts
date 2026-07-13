85c4c3ed053fc1895150ade0c1448c69fd42edcb
Abhinandh S <abhinandhsuby@proton.me>
2026-07-13 22:48:53 +0530

    Update render.ts

--- src/render.ts
+++ src/render.ts
@@ -5,19 +5,15 @@ export function renderLevel(id: string): string {
 }
 
 export function renderSet(id: string): string {
-  return id === "s1"
-    ? "set: 1"
-    : id === "s2"
-    ? "set: 2"
-    : id === "s1a"
-    ? "set: 1 solution"
-    : id === "s2a"
-    ? "set: 2 solution"
-    : id === "q"
-    ? "Question Paper"
-    : id === "a"
-    ? "Answer Key"
-    : id;
+  return
+    id === "s1" ? "set: 1" :
+    id === "s2" ? "set: 2" :
+    id === "s1a" ? "set: 1 solution" :
+    id === "s2a" ? "set: 2 solution":
+    id === "q" ? "Question Paper" :
+    id === "a" ? "Answer Key" :
+    id === "sa" ? "Suggested Answer" :
+    id;
 }
 
 export function renderSyllabus(ctx: string): string {
