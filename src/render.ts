a7f78205fcd2d0b80baf95c3163da578ec8ccfc7
Abhinandh S <abhinandhsuby@proton.me>
2026-07-13 22:37:08 +0530

    Update render.ts

--- src/render.ts
+++ src/render.ts
@@ -3,3 +3,25 @@ import { paperIdToLevel } from "./db/mod.ts";
 export function renderLevel(id: string): string {
   return `<blockquote>CMA ${paperIdToLevel(id).toUpperCase()}</blockquote>`;
 }
+
+export function renderSet(id: string): string {
+  return id === "s1"
+    ? "set: 1"
+    : id === "s2"
+    ? "set: 2"
+    : id === "s1a"
+    ? "set: 1 solution"
+    : id === "s2a"
+    ? "set: 2 solution"
+    : id === "q"
+    ? "Question Paper"
+    : id === "a"
+    ? "Answer Key"
+    : id;
+}
+
+function renderSyllabus(ctx: string): string {
+  return ctx === "syl16" ? "syllabus: 2016"
+    : ctx === "syl22" ? "syllabus: 2022"
+    : ctx;
+}
