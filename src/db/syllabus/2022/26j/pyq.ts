b5e355348cf1b7ef446dd3fbcb8ff6d9502f0743
Abhinandh S <abhinandhsuby@proton.me>
2026-07-14 20:29:37 +0530

    Update pyq.ts

--- src/db/syllabus/2022/26j/pyq.ts
+++ src/db/syllabus/2022/26j/pyq.ts
@@ -1,3 +1,5 @@
+import { FileRecord } from "../../../mod.ts";
+
 export const TERM_26J: Record<string, FileRecord> = {
     "p1-26j-pyq": [{ name: "q", id: "BQACAgUAAxkDAAI-gmpWTeTw0p_91onnaa-7oFQa42rKAAKOKQACLHuxVrxw2NS5GzqfPQQ", syllabus: "2022" }],
   "p2-26j-pyq": [{ name: "q", id: "BQACAgUAAxkDAAI-hGpWTeYkbK-vYzi3AZmF6VGKuEZHAAKPKQACLHuxVj2GbW1fUkTaPQQ", syllabus: "2022" }],
