36d9cf74992eab441959f4646afc5cf07bc958b4
Abhinandh S <abhinandhsuby@proton.me>
2026-07-14 23:39:34 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { Bot, Context, InlineKeyboard, webhookCallback } from "grammy";
-import { ACADEMIC_DATA, DocType, FileRecord, getAllFiles, getFiles, Level, MQP_FILE_IDS, PYQ_FILE_IDS } from "./db/mod.ts";
+import { ACADEMIC_DATA, DocType, FileRecord, getAllFiles, getFiles, Level, PYQ_FILE_IDS } from "./db/mod.ts";
 import { helpCmd } from "./cmd/help.ts";
 import { batchCmd } from "./cmd/batch.ts";
 import { inlineQueryHandler } from "./inline.ts";
