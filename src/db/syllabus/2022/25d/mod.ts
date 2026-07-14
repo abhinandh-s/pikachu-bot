385e48a7e2ad9286691dbf027d3e63a3f1242f37
Abhinandh S <abhinandhsuby@proton.me>
2026-07-14 20:59:47 +0530

    Create mod.ts

---
+++ src/db/syllabus/2022/25d/mod.ts
@@ -0,0 +1,9 @@
+import { FILE_IDS as PYQS } from "./pyq.ts";
+// import { FILE_IDS as PTPS } from "./ptp.ts";
+// import { FILE_IDS as MQPS } from "./mqp.ts";
+
+export const TERM_25D = {
+  PYQS,
+  // PTPS,
+  // MQPS
+};
