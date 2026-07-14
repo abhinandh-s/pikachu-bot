db0a29e83b5d0e3e1043e003d04628fb610c7bdb
Abhinandh S <abhinandhsuby@proton.me>
2026-07-14 21:18:00 +0530

    Update mod.ts

--- src/db/syllabus/2022/25j/mod.ts
+++ src/db/syllabus/2022/25j/mod.ts
@@ -2,7 +2,7 @@ import { FILE_IDS as PYQS } from "./pyq.ts";
 // import { FILE_IDS as PTPS } from "./ptp.ts";
 // import { FILE_IDS as MQPS } from "./mqp.ts";
 
-export const TERM_25D = {
+export const TERM_25J = {
   PYQS,
   // PTPS,
   // MQPS
