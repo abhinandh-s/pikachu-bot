abea773df441469d90453ff4638ec8b22a8a735e
Abhinandh S <abhinandhsuby@proton.me>
2026-07-14 21:53:20 +0530

    Update mod.ts

--- src/db/syllabus/2022/mod.ts
+++ src/db/syllabus/2022/mod.ts
@@ -1,3 +1,5 @@
 export { TERM_26J } from "./26j/mod.ts";
 export { TERM_25D } from "./25d/mod.ts";
 export { TERM_25J } from "./25j/mod.ts";
+export { TERM_24D } from "./24d/mod.ts";
+export { TERM_24J } from "./24j/mod.ts";
