47069e3aae9f377aa6b2e081629448fc723cdddd
Abhinandh S <abhinandhsuby@proton.me>
2026-07-14 21:49:51 +0530

    Update mod.ts

--- src/db/syllabus/2016/mod.ts
+++ src/db/syllabus/2016/mod.ts
@@ -1,9 +1,1 @@
-import { FILE_IDS as PYQS } from "./pyqs.ts";
-// import { FILE_IDS as PTPS } from "./ptps.ts";
-// import { FILE_IDS as MQPS } from "./mqps.ts";
-
-export const TERM_23D = {
-  PYQS,
-  // PTPS,
-  // MQPS
-};
+export { TERM_23D } from "./23d/mod.ts";
