d87c189a397cd84ba8458879f3a1713f4055561d
Abhinandh S <abhinandhsuby@proton.me>
2026-07-14 23:44:42 +0530

    Create types.ts

---
+++ src/types.ts
@@ -0,0 +1,4 @@
+export type Level = "foundation" | "intermediate" | "final";
+export type DocType = "pyq" | "mqp" | "ptp";
+
+export type FileRecord = { name: string; id: string; syllabus?: string }[];
