9cbe6dbb44a8a65f61e641393301a7eb7e8de481
Abhinandh S <abhinandhsuby@proton.me>
2026-07-14 21:40:35 +0530

    Create templates/pyqs.ts

---
+++ src/db/syllabus/templates/pyqs.ts
@@ -0,0 +1,8 @@
+import { FileRecord } from "../../../mod.ts";
+
+export const FILE_IDS: Record<string, FileRecord> = {
+  "p[num]-[term]-pyq": [
+    { name: "q", id: "", syllabus: "" },
+    { name: "sa", id: "", syllabus: "" }
+  ],
+};
