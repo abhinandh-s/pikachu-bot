634e17b84feb19f635c678225023aedf07fb7a40
Abhinandh S <abhinandhsuby@proton.me>
2026-07-14 20:07:54 +0530

    Create 26j/ptp.ts

---
+++ src/db/syllabus/2022/26j/ptp.ts
@@ -0,0 +1,92 @@
+import { FileRecord } from "./mod.ts";
+
+export const FILE_IDS: Record<string, FileRecord> = {
+  "p1-26j-ptp": [
+    { name: "a", id: "BQACAgUAAxkDAAIn5WpU3DKARnvRDYWMTxTOy58GbfllAAJWKwACEmGpVg6Z1r17blKLPAQ" },
+    { name: "q", id: "BQACAgUAAxkDAAIoAWpU3Eklq4mFNqhh-qyY0fi7ykvaAAJkKwACEmGpVhlf6ED38QXgPAQ" }
+  ],
+  "p2-26j-ptp": [
+    { name: "a", id: "BQACAgUAAxkDAAIn52pU3DSxn_131WrQw4OC3GMVFgX_AAJXKwACEmGpVlV55i1QyOKhPAQ" },
+    { name: "q", id: "BQACAgUAAxkDAAIoA2pU3ErSe0IlTB0a6ugFDxCZ6EW0AAJlKwACEmGpVpd5rnF7mWB3PAQ" }
+  ],
+  "p3-26j-ptp": [
+    { name: "a", id: "BQACAgUAAxkDAAIn6WpU3DXWu7MOYl1F3e0rNaFaTQAB0gACWCsAAhJhqVbZtEbESi5ZiTwE" },
+    { name: "q", id: "BQACAgUAAxkDAAIoBWpU3Ey5pq9gzVdkB-grNFszY1gjAAJmKwACEmGpViUIk9aEKkibPAQ" }
+  ],
+  "p4-26j-ptp": [
+    { name: "a", id: "BQACAgUAAxkDAAIn62pU3DdlslPvTX5LEu89O1hntkgzAAJZKwACEmGpVumMl6jOl6wCPAQ" },
+    { name: "q", id: "BQACAgUAAxkDAAIoB2pU3E2BZR-t-Dt5wq8QlzfGTDjoAAJnKwACEmGpVtvpQ_akAQWSPAQ" }
+  ],
+  "p5-26j-ptp": [
+    { name: "a", id: "BQACAgUAAxkDAAIoHWpU3FxICrRddpFDtrtWpIV_qEuMAAJyKwACEmGpVkjMM8K8LA0IPAQ" },
+    { name: "q", id: "BQACAgUAAxkDAAIoLwUpU3GnR3lYCeOQfVmn79HD3L1s9AAJ6KwACEmGpVsfirvTPtwxqPAQ" }
+  ],
+  "p6-26j-ptp": [
+    { name: "a", id: "BQACAgUAAxkDAAIoH2pU3F6G29WlSVGtXtXMk67g3SdJAAJzKwACEmGpVpgjKJeTaGWTPAQ" },
+    { name: "q", id: "BQACAgUAAxkDAAIoL2pU3Gvy8leULP3UsAwhxDDrV8PJAAJ7KwACEmGpVoy-cSVpt6unPAQ" }
+  ],
+  "p7-26j-ptp": [
+    { name: "a", id: "BQACAgUAAxkDAAIoIWpU3F9f8rbSFyrxWB1hWdXYjMmtAAJ0KwACEmGpVocbbHzav2RpPAQ" },
+    { name: "q", id: "BQACAgUAAxkDAAIoMWpU3GyEReKqm_MDpzuHl-Zg9Qi7AAJ8KwACEmGpVomxem8T2lWGPAQ" }
+  ],
+  "p8-26j-ptp": [
+    { name: "a", id: "BQACAgUAAxkDAAIoI2pU3GDYubYwJMUdaiT8mNkHFPHtAAJ1KwACEmGpVolv5xlvO_HoPAQ" },
+    { name: "q", id: "BQACAgUAAxkDAAIoM2pU3G4uAn7PDLJOuQ4jEKGSx18BAAJ9KwACEmGpVsvbWQL3U9bUPAQ" }
+  ],
+  "p9-26j-ptp": [
+    { name: "a", id: "BQACAgUAAxkDAAIoJWpU3GJmiH3Q-YFtM83MIrN6Rl9SAAJ2KwACEmGpVsoVFDp4kcGNPAQ" },
+    { name: "q", id: "BQACAgUAAxkDAAIoNWpU3G_JNkyqKtmhHLzPdUDPEBUTAAJ-KwACEmGpVo5BHOtKY0wgPAQ" }
+  ],
+  "p10-26j-ptp": [
+    { name: "a", id: "BQACAgUAAxkDAAIoJ2pU3GVln4SqjaWnoFHfKvKie3zNAAJ3KwACEmGpVnEw6-YtLBwIPAQ" },
+    { name: "q", id: "BQACAgUAAxkDAAIoN2pU3HE8fEAkabeUo5JkUvziq1_rAAJ_KwACEmGpVhmgvJUOaB8ZPAQ" }
+  ],
+  "p11-26j-ptp": [
+    { name: "a", id: "BQACAgUAAxkDAAIoKWpU3GZNeJrgz_BLM463PYduTzxJAAJ4KwACEmGpVqn1MIDTXSaePAQ" },
+    { name: "q", id: "BQACAgUAAxkDAAIoOWpU3HJ8rgjhXMVdCTFMk9YP6WwpAAKAKwACEmGpVoY9Q8D2PFWAPAQ" }
+  ],
+  "p12-26j-ptp": [
+    { name: "a", id: "BQACAgUAAxkDAAIoK2pU3GjcBDmenybNvf09HEj800kyAAJ5KwACEmGpVjWcuZs6tZpvPAQ" },
+    { name: "q", id: "BQACAgUAAxkDAAIoO2pU3HRBohwIjzoCabUlWk_1a5LeAAKBKwACEmGpVl6Xo5HTTSjGPAQ" }
+  ],
+  "p13-26j-ptp": [
+    { name: "a", id: "BQACAgUAAxkDAAIn7WpU3Dn6EIhfNxw4B_tYDaIkVmx8AAJaKwACEmGpVvqjT50KkVViPAQ" },
+    { name: "q", id: "BQACAgUAAxkDAAIoCWpU3E5MeiZ9OZxV5lHZ8UgvzFGIAAJoKwACEmGpVrHnO-4K1iT3PAQ" }
+  ],
+  "p14-26j-ptp": [
+    { name: "a", id: "BQACAgUAAxkDAAIn72pU3DpkDatzaopPiDrh3OYNEKzAAAJbKwACEmGpVqItcQkYA-g2PAQ" },
+    { name: "q", id: "BQACAgUAAxkDAAIoC2pU3FBsdYZcvwIq1Uk-NLMjwwelAAJpKwACEmGpVkHhqQqzLJrSPAQ" }
+  ],
+  "p15-26j-ptp": [
+    { name: "a", id: "BQACAgUAAxkDAAIn8WpU3Dwc3jFuuQgjxkUFhKq6PsoLAAJcKwACEmGpVt60tBpVsSkePAQ" },
+    { name: "q", id: "BQACAgUAAxkDAAIoDWpU3FFV7MvIvqvSPl5DssQ-oRqFAAJqKwACEmGpVn7MXQoGntBsPAQ" }
+  ],
+  "p16-26j-ptp": [
+    { name: "a", id: "BQACAgUAAxkDAAIn82pU3D0Wm1hV8Yvy3KpRUF_jRc_gAAJdKwACEmGpVt-60VEZLfO9PAQ" },
+    { name: "q", id: "BQACAgUAAxkDAAIoD2pU3FJX1Fl5Pu9qLwaKgDWxsht_AAJrKwACEmGpVu6lKaWGnkZzPAQ" }
+  ],
+  "p17-26j-ptp": [
+    { name: "a", id: "BQACAgUAAxkDAAIn9WpU3D-JXDEkkCR6ByegfRVz5aB6AAJeKwACEmGpVuHxOIbOpQI0PAQ" },
+    { name: "q", id: "BQACAgUAAxkDAAIoEWpU3FTJT-oJk4qhmjJDb1gmRs8DAAJsKwACEmGpVg5xbQSJKiZtPAQ" }
+  ],
+  "p18-26j-ptp": [
+    { name: "a", id: "BQACAgUAAxkDAAIn92pU3EHj-i5PIjVNbZXa4cTie-v_AAJfKwACEmGpVsvd-zrg6oYiPAQ" },
+    { name: "q", id: "BQACAgUAAxkDAAIoE2pU3FZKKJZQQgiLPGiwFLG-0LQGAAJtKwACEmGpVuiYvAABHmuWDzwE" }
+  ],
+  "p19-26j-ptp": [
+    { name: "a", id: "BQACAgUAAxkDAAIn-WpU3EP0FCVZpdqEJ90WVamM6LIMAAJgKwACEmGpVlxy3WAAAXT9vzwE" },
+    { name: "q", id: "BQACAgUAAxkDAAIoFWpU3FdNc3B7XthmB4uc7hvTh0PWAAJuKwACEmGpVroavThyp5gYPAQ" }
+  ],
+  "p20A-26j-ptp": [
+    { name: "a", id: "BQACAgUAAxkDAAIn-2pU3ETuQZsrMuqzQgHRC1vRpH7AAAJhKwACEmGpVv8zBWK2h8f_PAQ" },
+    { name: "q", id: "BQACAgUAAxkDAAIoF2pU3Fi45cnCAAF6xi-pWRodvWGCuQACbysAAhJhqVaHU5dWUKgaPDwE" }
+  ],
+  "p20B-26j-ptp": [
+    { name: "a", id: "BQACAgUAAxkDAAIn_WpU3EWtE1OUN3JgK5K5q_6y9q2dAAJiKwACEmGpVp2RtpEleMPZPAQ" },
+    { name: "q", id: "BQACAgUAAxkDAAIoGWpU3FpKLu9eY4tL8TICCEJRqmgbAAJwKwACEmGpVlXSUFizQZyrPAQ" }
+  ],
+  "p20C-26j-ptp": [
+    { name: "a", id: "BQACAgUAAxkDAAIn_2pU3Ed9vMusYcRDTISqQ9IT9L8cAAJjKwACEmGpVqfrDefdpxOePAQ" },
+    { name: "q", id: "BQACAgUAAxkDAAIoG2pU3FsEkRAAAZmGJpcIrkDRrFUkIQACcSsAAhJhqVa1jV1JQ7-QxjwE" }
+  ]
+};
