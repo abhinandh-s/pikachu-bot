e17d2f858bdabea9838ec935583dc9d483254c40
Abhinandh S <abhinandhsuby@proton.me>
2026-07-14 21:52:24 +0530

    Create 24d/mod.ts

---
+++ src/db/syllabus/2022/24d/mod.ts
@@ -0,0 +1,9 @@
+import { FILE_IDS as PYQS } from "./pyqs.ts";
+// import { FILE_IDS as PTPS } from "./ptps.ts";
+// import { FILE_IDS as MQPS } from "./mqps.ts";
+
+export const TERM_24D = {
+  PYQS,
+  // PTPS,
+  // MQPS
+};
