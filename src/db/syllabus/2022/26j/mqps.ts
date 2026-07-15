87c18fdd8fa1258a04fde721c3997f6f59d4c457
Abhinandh S <abhinandhsuby@proton.me>
2026-07-15 18:06:10 +0530

    Create mqps.ts

---
+++ src/db/syllabus/2022/26j/mqps.ts
@@ -0,0 +1,118 @@
+import { FileRecord } from "../../../mod.ts";
+
+export const TERM_26J: Record<string, FileRecord[]> = {
+  "p1-26j-mqp": [
+    { name: "s1", id: "BQACAgUAAxkDAAJEUWpXfI7ZZm6qPtA4IR3vDA3xAAF3tgACAiIAAix7wVbTeohy4flsVD0E", syllabus: "2022" },
+    { name: "s1a", id: "BQACAgUAAxkDAAJEe2pXfLTfC4OHnnm7h_aaXogb-hLzAAIXIgACLHvBVooMdggvFEprPQQ", syllabus: "2022" },
+    { name: "s2", id: "BQACAgUAAxkDAAJEu2pXfPBk_Ozo09W6YF3A0bQxVdhdAAI3IgACLHvBVtNPxeVKd0WPPQQ", syllabus: "2022" },
+    { name: "s2a", id: "BQACAgUAAxkDAAJEpWpXfNvqm5-fW8f2jXqWrrREfLWOAAIsIgACLHvBVlfCeOfRzXc2PQQ", syllabus: "2022" }
+  ],
+  "p2-26j-mqp": [
+    { name: "s1", id: "BQACAgUAAxkDAAJEU2pXfJDpN3JnCTe32u32emAVz8o-AAIDIgACLHvBVrKLSSVLxKrDPQQ", syllabus: "2022" },
+    { name: "s1a", id: "BQACAgUAAxkDAAJEfWpXfLYEp2AuOUEN88xAT2JVDB7jAAIYIgACLHvBVo2CTduIUpYfPQQ", syllabus: "2022" },
+    { name: "s2", id: "BQACAgUAAxkDAAJEvWpXfPGDGeoAAUjRg0MGrgWi0gPqlAACOCIAAix7wVZVzhI3Un_UoD0E", syllabus: "2022" },
+    { name: "s2a", id: "BQACAgUAAxkDAAJEp2pXfNyqvrh9y9lzCk8xV0E4d1aFAAItIgACLHvBVnRFqlhIYhq1PQQ", syllabus: "2022" }
+  ],
+  "p3-26j-mqp": [
+    { name: "s1", id: "BQACAgUAAxkDAAJEVWpXfJKc_mh5qLtzz-mY8j5OjQgwAAIEIgACLHvBVravdudKVT1QPQQ", syllabus: "2022" },
+    { name: "s1a", id: "BQACAgUAAxkDAAJEf2pXfLgodxtTwiyF9tofk9s88weKAAIZIgACLHvBVuVOUDDgcGoBPQQ", syllabus: "2022" },
+    { name: "s2", id: "BQACAgUAAxkDAAJEv2pXfPPuHWwQNjVTgDcroK070RoVAAI5IgACLHvBVipC5N_BQ2A1PQQ", syllabus: "2022" },
+    { name: "s2a", id: "BQACAgUAAxkDAAJEqWpXfN45PeA-vU6VKr2mAgh3cY9UAAIuIgACLHvBVhzqCXrRDB1RPQQ", syllabus: "2022" }
+  ],
+  "p4-26j-mqp": [
+    { name: "s1", id: "BQACAgUAAxkDAAJEV2pXfJRS0jrAwZjrZlC2CwzaeYONAAIFIgACLHvBVr1gEFBMZy8CPQQ", syllabus: "2022" },
+    { name: "s1a", id: "BQACAgUAAxkDAAJEgWpXfLmnueK0UkPvCfj0CHUr1fQ1AAIaIgACLHvBVucWRxGCUww-PQQ", syllabus: "2022" },
+    { name: "s2", id: "BQACAgUAAxkDAAJEwWpXfPUHmNlygDahPm5Ty240vvDFAAI6IgACLHvBVm-oUbn-lDNjPQQ", syllabus: "2022" },
+    { name: "s2a", id: "BQACAgUAAxkDAAJEq2pXfOAsmrlm93bI3PMzpbJGVn9QAAIvIgACLHvBVocCbBQyUzVlPQQ", syllabus: "2022" }
+  ],
+  "p5-26j-mqp": [
+    { name: "s1", id: "BQACAgUAAxkDAAJE0WpXfQKMUFMAAaL3WzKEqAFJSbkAAbUAAkIiAAIse8FWzTLGRrv6yf49BA", syllabus: "2022" },
+    { name: "s1a", id: "BQACAgUAAxkDAAJE02pXfQQwt_Mjx4zG92iucqUDMg7zAAJDIgACLHvBVoPiV2qMnDAQPQQ", syllabus: "2022" },
+    { name: "s2", id: "BQACAgUAAxkDAAJE1WpXfQjNl5-DDv3anX0YqmbsP8AKAAJEIgACLHvBVhf5-fJwL4CIPQQ", syllabus: "2022" },
+    { name: "s2a", id: "BQACAgUAAxkDAAJE12pXfQrDu6jpGpc2tdT6Tc4xlSD2AAJFIgACLHvBVqyJmuXjNb6vPQQ", syllabus: "2022" }
+  ],
+  "p6-26j-mqp": [
+    { name: "s1", id: "BQACAgUAAxkDAAJEWWpXfJbNlzU4nkTxizrLtC6Ffk12AAIGIgACLHvBVptgjTJT5AVcPQQ", syllabus: "2022" },
+    { name: "s1a", id: "BQACAgUAAxkDAAJEg2pXfLukCcY5R-MYzZwuRnPVfdn6AAIbIgACLHvBVttosfAS3KQBPQQ", syllabus: "2022" },
+    { name: "s2", id: "BQACAgUAAxkDAAJEw2pXfPYCW7O73sspDeXrkS90AY2UAAI7IgACLHvBVjhyEbacNqafPQQ", syllabus: "2022" },
+    { name: "s2a", id: "BQACAgUAAxkDAAJErWpXfOLOIjpbAQ4xlRe5AvHhEQO5AAIwIgACLHvBVjk8GaTQm51jPQQ", syllabus: "2022" }
+  ],
+  "p7-26j-mqp": [
+    { name: "s1", id: "BQACAgUAAxkDAAJEW2pXfJdtkyY_X4RtOt7qOof2wMC-AAIHIgACLHvBVkFCxDOI_v5yPQQ", syllabus: "2022" },
+    { name: "s1a", id: "BQACAgUAAxkDAAJEhWpXfL3RhGDPG_aQHL64CyV-4CyeAAIcIgACLHvBVrq4RbYPdAp6PQQ", syllabus: "2022" },
+    { name: "s2", id: "BQACAgUAAxkDAAJExWpXfPhI1VrFz9_FUYvB62aM5ewEAAI8IgACLHvBVrBaDDl8FARnPQQ", syllabus: "2022" },
+    { name: "s2a", id: "BQACAgUAAxkDAAJEr2pXfOSjksJinq6Ch6n-aOAJbqYpAAIxIgACLHvBVj8RfDoSf8ZhPQQ", syllabus: "2022" }
+  ],
+  "p8-26j-mqp": [
+    { name: "s1", id: "BQACAgUAAxkDAAJEXWpXfJlsVLmjoWUpyyRvgLj24LUWAAIIIgACLHvBVhOrCMWVxIrpPQQ", syllabus: "2022" },
+    { name: "s1a", id: "BQACAgUAAxkDAAJEh2pXfL_yq2RVY19iAAGnoK8yR1seiAACHSIAAix7wVa_VdWWbGjS3T0E", syllabus: "2022" },
+    { name: "s2", id: "BQACAgUAAxkDAAJEx2pXfPoUY29yXvlBGj81qNEBmGyEAAI9IgACLHvBVn6YOECWf8muPQQ", syllabus: "2022" },
+    { name: "s2a", id: "BQACAgUAAxkDAAJEsWpXfOYNK3tOPYyVZ_PRnknEQI_bAAIyIgACLHvBVoWAOImA2XR6PQQ", syllabus: "2022" }
+  ],
+  "p9-26j-mqp": [
+    { name: "s1", id: "BQACAgUAAxkDAAJEX2pXfJv1uR4KC2hkjorKW01tGRGHAAIJIgACLHvBVoKhDVVI-V30PQQ", syllabus: "2022" },
+    { name: "s1a", id: "BQACAgUAAxkDAAJEiWpXfMHm1pEOxu4JPBFGlY0uhYETAAIeIgACLHvBVndhfjqncZ_ZPQQ", syllabus: "2022" },
+    { name: "s2", id: "BQACAgUAAxkDAAJEyWpXfPy5uI8-IEIG-IKcr12zEe7_AAI-IgACLHvBVsuP70T-JY09PQQ", syllabus: "2022" },
+    { name: "s2a", id: "BQACAgUAAxkDAAJEs2pXfOhZ5QuvrLXZBl_QVp7KoSyQAAIzIgACLHvBVjxE-Mm9DBBbPQQ", syllabus: "2022" }
+  ],
+  "p10-26j-mqp": [
+    { name: "s1", id: "BQACAgUAAxkDAAJEYWpXfJzQhRGamzh1Ha5392fxGCnuAAIKIgACLHvBVuFjWa7xFzD0PQQ", syllabus: "2022" },
+    { name: "s1a", id: "BQACAgUAAxkDAAJEi2pXfMPzA2MG67wnfyzaNwLddUREAAIfIgACLHvBVuzStxH9_x4sPQQ", syllabus: "2022" },
+     { name: "s2", id: "BQACAgUAAxkDAAJE4GpXfnAxF3v9tu35YPetxPcsuv1eAAJPIgACLHvBVl2peOr1NaR-PQQ", syllabus: "2022" },
+    { name: "s2a", id: "BQACAgUAAxkDAAJEtWpXfOo-SxC3s5ykO7y46uQM7BODAAI0IgACLHvBVlbFSvWC4xwdPQQ", syllabus: "2022" }
+  ],
+  "p11-26j-mqp": [
+    { name: "s1", id: "BQACAgUAAxkDAAJEY2pXfJ4K3Lkkr7cv93bOAs0XjWBOAAILIgACLHvBVkKenDQ272W3PQQ", syllabus: "2022" },
+    { name: "s1a", id: "BQACAgUAAxkDAAJEjWpXfMQmcUPsyNyC714862O9n9_0AAIgIgACLHvBVt0FzoNeZiD6PQQ", syllabus: "2022" },
+    { name: "s2", id: "BQACAgUAAxkDAAJEzWpXfP9dmCY-BoKWHrBW5VXbAAE4mAACQCIAAix7wVYRT_1d72SX8D0E", syllabus: "2022" },
+    { name: "s2a", id: "BQACAgUAAxkDAAJEt2pXfOxxVaNkyrrkcTux5wsbNXg-AAI1IgACLHvBViSJoFV3uwRHPQQ", syllabus: "2022" }
+  ],
+  "p12-26j-mqp": [
+    { name: "s1", id: "BQACAgUAAxkDAAJEZWpXfKCivdu_PoMytaIEumuqsR30AAIMIgACLHvBVg5VsFQV_M3oPQQ", syllabus: "2022" },
+    { name: "s1a", id: "BQACAgUAAxkDAAJEj2pXfMZ0Id97WI264xjbdd8qzzpkAAIhIgACLHvBVohA2LWo75JpPQQ", syllabus: "2022" },
+    { name: "s2", id: "BQACAgUAAxkDAAJEz2pXfQABfr_mj4ZaWNlMJyxRVW4RXAACQSIAAix7wVYpr2VvPyQ9pj0E", syllabus: "2022" },
+    { name: "s2a", id: "BQACAgUAAxkDAAJEuWpXfO6FJYrKsbrC3WB-82qrnrewAAI2IgACLHvBVn6CguN-w2iFPQQ", syllabus: "2022" }
+  ],
+  "p13-26j-mqp": [
+    { name: "s1", id: "BQACAgUAAxkDAAJEZ2pXfKKqeAQ2WWcFpZRLMEJnUb2OAAINIgACLHvBVoFOuypRLQG0PQQ", syllabus: "2022" },
+    { name: "s1a", id: "BQACAgUAAxkDAAJEkWpXfMi2b1CWkwrIMEQSoUlwazQAAyIiAAIse8FWzTLGRrv6yf49BA", syllabus: "2022" }
+  ],
+  "p14-26j-mqp": [
+    { name: "s1", id: "BQACAgUAAxkDAAJEaWpXfKPAD-NDqRKNezxJbETnKzblAAIOIgACLHvBVjEWy0apEeNpPQQ", syllabus: "2022" },
+    { name: "s1a", id: "BQACAgUAAxkDAAJEk2pXfMo5Ab-vaBNHS653oVsBbFh9AAIjIgACLHvBVqyFKVDS0BiUPQQ", syllabus: "2022" }
+  ],
+  "p15-26j-mqp": [
+    { name: "s1", id: "BQACAgUAAxkDAAJEa2pXfKUVW7Vb4MtRvif2pkBtW85OAAIPIgACLHvBVqsuub_zfu1APQQ", syllabus: "2022" },
+    { name: "s1a", id: "BQACAgUAAxkDAAJElWpXfMytSVr-PsK-UDrYjUEdY-isAAIkIgACLHvBVl-tCk9jR7aTPQQ", syllabus: "2022" }
+  ],
+  "p16-26j-mqp": [
+    { name: "s1", id: "BQACAgUAAxkDAAJEbWpXfKdMu8kRuwuIb5mki5cLCNExAAIQIgACLHvBVkfCr5k2DcPNPQQ", syllabus: "2022" },
+    { name: "s1a", id: "BQACAgUAAxkDAAJEl2pXfM7Lv9hYfns3VlRumWfvVjK_AAIlIgACLHvBVrup2eqDej9vPQQ", syllabus: "2022" }
+  ],
+  "p17-26j-mqp": [
+    { name: "s1", id: "BQACAgUAAxkDAAJEb2pXfKlhASS22Z3aW6KQE9eRrffBAAIRIgACLHvBVvb6sDCttE7fPQQ", syllabus: "2022" },
+    { name: "s1a", id: "BQACAgUAAxkDAAJEmWpXfNBSfI0JHjk-yFaIGWtHUqqcAAImIgACLHvBVrlrV0gngKquPQQ", syllabus: "2022" }
+  ],
+  "p18-26j-mqp": [
+    { name: "s1", id: "BQACAgUAAxkDAAJEcWpXfKu84VDor7ey6CT0d1fafv6KAAISIgACLHvBVnW8RYHy066DPQQ", syllabus: "2022" },
+    { name: "s1a", id: "BQACAgUAAxkDAAJEm2pXfNFyiERvFHew4AjpFq8W0UdXAAInIgACLHvBVluR_qGdy3ZkPQQ", syllabus: "2022" }
+  ],
+  "p19-26j-mqp": [
+    { name: "s1", id: "BQACAgUAAxkDAAJEc2pXfKwvlEvJCwtDEGzwNdwC0UvnAAITIgACLHvBVs7wSMknKTqhPQQ", syllabus: "2022" },
+    { name: "s1a", id: "BQACAgUAAxkDAAJEnWpXfNMCO5H1TShwMpI4Kg-V-lpqAAIoIgACLHvBVgXRDkgB6uhQPQQ", syllabus: "2022" }
+  ],
+  "p20-26j-mqp": [
+    { name: "s1a", id: "BQACAgUAAxkDAAJEn2pXfNV9MitqxYxT6ybhfBVL5HUrAAIpIgACLHvBVh1JaPM7qHZ3PQQ", syllabus: "2022" }
+  ],
+  "p20A-26j-mqp": [
+    { name: "s1", id: "BQACAgUAAxkDAAJEdWpXfK7w2zrYfV-J-u8OA9KldpYQAAIUIgACLHvBVuQzzJPfihipPQQ", syllabus: "2022" }
+  ],
+  "p20B-26j-mqp": [
+    { name: "s1", id: "BQACAgUAAxkDAAJEd2pXfLBXpVneNx3eZrhvt2aRpG7aAAIVIgACLHvBVmzzly8CKoYUPQQ", syllabus: "2022" },
+    { name: "s1a", id: "BQACAgUAAxkDAAJEoWpXfNdCTV1dlcyS9mL46aSQ6tdTAAIqIgACLHvBVvJZpHusOdJkPQQ", syllabus: "2022" }
+  ],
+  "p20C-26j-mqp": [
+    { name: "s1", id: "BQACAgUAAxkDAAJEeWpXfLIVUaqL8fqVLdpHmQVkqhzUAAIWIgACLHvBVgr7v6bXxt31PQQ", syllabus: "2022" },
+    { name: "s1a", id: "BQACAgUAAxkDAAJEo2pXfNlKfsEeVW0UvacPfGYdlBFXAAIrIgACLHvBVrS1KqblyBsYPQQ", syllabus: "2022" }
+  ],
+};
