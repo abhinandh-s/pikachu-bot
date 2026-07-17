973a78e32b7ea2fadd06cd5c3fe69ec4577cd4a1
Abhinandh S <abhinandhsuby@proton.me>
2026-07-17 12:06:00 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { adminCmds } from "./cmd/admin.ts";
 import { batchCmd } from "./cmd/batch.ts";
 import { inlineQueryHandler } from "./inline.ts";
 import { formatTerm } from "./utils.ts";
-import { renderCaption } from "./render.ts";
+import { renderCaption, renderSyllabusShort } from "./render.ts";
 
 import { FLATTENED_FILE_IDS } from "./db/mod.ts";
 
@@ -34,7 +34,7 @@ function buildSearchKeyboard(query: string, page: number) {
     const [paperId, term, docType, fileName, syl] = key.split("-");
 
     // E.g., "P20C 26J | MQP S1A | SYL22"
-    const btnText = `${paperId.toUpperCase()} - ${formatTerm(term)} | ${docType.toUpperCase()} ${fileName.toUpperCase()} | ${syl.toUpperCase()}`;
+    const btnText = `${paperId.toUpperCase()} - ${formatTerm(term)} | ${docType.toUpperCase()} ${fileName.toUpperCase()} | SYLLABUS ${renderSyllabusShort(syl)}`;
 
     // Pass the full key! (Length is ~22 bytes, well under 64-byte limit)
     keyboard.text(btnText, `dl:${key}`).row();
