e28839ef266f112a6a370a804508d560e26a9c2e
Abhinandh S <abhinandhsuby@proton.me>
2026-07-20 15:23:18 +0530

    Update help.ts

--- src/cmd/help.ts
+++ src/cmd/help.ts
@@ -139,7 +139,7 @@ helpCmd.command("status", async (ctx) => {
 
 helpCmd.command("privacy", async (ctx) => {
   await ctx.reply(
-    "<b>Privacy policy</b>
+    `<b>Privacy policy</b>
 
 I gain absolutely <b>zero</b> monetary benefit from this. This is a passion project and I <b>do not</b> collect any user data.
 
@@ -148,7 +148,7 @@ I gain absolutely <b>zero</b> monetary benefit from this. This is a passion proj
 <i>Pikachu is an independent study tool and is not officially affiliated with the Institute of Cost Accountants of India (ICMAI). All rights to the PDFs and study materials are reserved by ICMAI.</i>
 
 <i>© 2026 icmai.in. All Rights Reserved.</i>
-",
+`,
     {
       parse_mode: "HTML"
     }
